f2891ac04c80bb016b55803d669d0e4210a95389
amrn1991 <amrfst91@gmail.com>
2025-03-10 14:38:45 +0330

    added shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1 className="text-3xl">صفحه اصلی</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl">صفحه اصلی</h1>
+      <Button variant="outline" size="lg" className="m-8">کلیک کنید</Button>
+    </div>
+  );
 }
---
+++ components/navigation/DropDown.tsx
@@ -0,0 +1,8 @@
+
+function DropDown() {
+  return (
+    <div>DropDown</div>
+  )
+}
+
+export default DropDown
